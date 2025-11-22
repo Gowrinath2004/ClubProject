06f712bf9739a1d45ac3fd77a1c3cf9865479502
Gowrinath2004 <gnariset@gitam.in>
2025-11-22 09:56:40 +0530

    fifth

---
+++ Backend/createuser.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const User = require("./models/user");
+
+mongoose.connect("mongodb://localhost:27017/campus_clubs")
+.then(async () => {
+    const hashedPassword = await bcrypt.hash("123456", 10);
+
+    await User.create({
+        email: "student@gmail.com",
+        password: hashedPassword
+    });
+
+    console.log("User Created Successfully");
+    mongoose.disconnect();
+});
